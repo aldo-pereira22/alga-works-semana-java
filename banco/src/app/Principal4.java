9885728210dcfe69c5d11cac3ab2305a1cdbad3b
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-09-16 20:26:50 -0300

    STREAM API: Ordenando fluxo

--- banco/src/app/Principal4.java
+++ banco/src/app/Principal4.java
@@ -4,6 +4,7 @@ import modelo.Banco;
 import modelo.Conta;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class Principal4 {
@@ -23,8 +24,9 @@ public class Principal4 {
 //        });
         Banco banco = new Banco();
         banco.getContas().stream()
-                .filter(conta-> conta.getSaldo().compareTo(new BigDecimal("130")) > 0)
-                .filter(conta -> conta.getNumero()>300)
+                .filter(conta-> conta.getSaldo().compareTo(new BigDecimal("50")) > 0)
+                .filter(conta -> conta.getNumero()>200)
+                .sorted(Comparator.comparingInt(Conta::getNumero))
                 .forEach(conta -> {
                                 System.out.println(conta.getAgencia()+" / " +
                     "" +conta.getNumero() +" = " + conta.getSaldo());
