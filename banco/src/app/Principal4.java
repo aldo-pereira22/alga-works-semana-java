5d35892d6bc1d41adc31501a16f6e05f653ce246
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-09-16 20:22:26 -0300

    STREAM API: Concatenando operações no fluxo stream

--- banco/src/app/Principal4.java
+++ banco/src/app/Principal4.java
@@ -8,18 +8,26 @@ import java.util.stream.Stream;
 
 public class Principal4 {
     public static void main(String[] args) {
+//        Banco banco = new Banco();
+//        Stream<Conta> stream =  banco.getContas().stream();
+//        Stream<Conta> stream2 =  stream.filter(
+//                                        conta -> conta.getSaldo()
+//                                                .compareTo(new BigDecimal("130")) > 0 );
+//
+//        Stream<Conta> stream3 = stream2.filter(conta -> conta.getNumero() > 300);
+//
+//        stream3.forEach(conta -> {
+//
+//            System.out.println(conta.getAgencia()+" / " +
+//                    "" +conta.getNumero() +" = " + conta.getSaldo());
+//        });
         Banco banco = new Banco();
-        Stream<Conta> stream =  banco.getContas().stream();
-        Stream<Conta> stream2 =  stream.filter(
-                conta -> conta.getSaldo().compareTo(new BigDecimal("130")) > 0
-        );
-
-        stream2.forEach(conta -> {
-
-            System.out.println(conta.getAgencia()+" / " +
+        banco.getContas().stream()
+                .filter(conta-> conta.getSaldo().compareTo(new BigDecimal("130")) > 0)
+                .filter(conta -> conta.getNumero()>300)
+                .forEach(conta -> {
+                                System.out.println(conta.getAgencia()+" / " +
                     "" +conta.getNumero() +" = " + conta.getSaldo());
-        });
-//        banco.getContas().get(0).setSaldo(new BigDecimal("100"));
-//        System.out.println("Saldo: "+banco.getContas().get(0).getSaldo());
+                });
     }
 }
