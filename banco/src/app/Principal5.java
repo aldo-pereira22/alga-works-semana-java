5ae964066d737dc77273571365c402054e5eb725
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-09-17 15:35:10 -0300

    Trabalhando com OPTIONAL

--- banco/src/app/Principal5.java
+++ banco/src/app/Principal5.java
@@ -3,6 +3,7 @@ package app;
 import modelo.Banco;
 import modelo.Conta;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 public class Principal5 {
@@ -10,13 +11,27 @@ public class Principal5 {
     public static void main(String[] args) {
         Banco banco = new Banco();
 
-        Optional<Conta> contaOptional =  banco.buscar(222,222);
+//        Optional<Conta> contaOptional =  banco.buscar(222,222);
+//        contaOptional.ifPresent(conta -> {
+//            System.out.println(conta.getSaldo());
+//        });
 
-        if(contaOptional.isPresent()){
+//        banco.buscar(222, 222)
+//                .ifPresent(conta -> System.out.println(conta.getSaldo()));
 
-            Conta contaEncontrada = contaOptional.get();
-            System.out.println(contaEncontrada.getSaldo());
-        }
+//        if(contaOptional.isPresent()){
+//
+//            Conta contaEncontrada = contaOptional.get();
+//            System.out.println(contaEncontrada.getSaldo());
+//        }
 
+//        Conta contaEncontrada = banco.buscar(2222, 222)
+//                .orElseThrow(() -> new RuntimeException("Conta não encontrada!"));
+
+        BigDecimal saldo = banco.buscar(222, 2222)
+                .map(conta -> conta.getSaldo())
+                .orElse(BigDecimal.ZERO);
+
+        System.out.println(saldo);
     }
 }
