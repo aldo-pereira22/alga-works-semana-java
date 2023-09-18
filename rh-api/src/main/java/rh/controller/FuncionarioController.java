7e320fbec8beb4dedbc617b778cf0ea66b193154
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-09-18 12:22:50 -0300

    Método Para listar funcionarios

--- rh-api/src/main/java/rh/controller/FuncionarioController.java
+++ rh-api/src/main/java/rh/controller/FuncionarioController.java
@@ -2,9 +2,12 @@ package rh.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import rh.model.Funcionario;
 import rh.repository.FuncionarioRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/funcionarios")
@@ -12,15 +15,17 @@ public class FuncionarioController {
 
     @Autowired
     private FuncionarioRepository funcionarioRepository;
-
-    @GetMapping
-    public String testar(){
-        return "Testando API";
-    }
+    
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Funcionario cadastrar(@RequestBody Funcionario funcionario){
         return funcionarioRepository.save(funcionario);
+    }
+
+    @GetMapping
+    public List<Funcionario> listar(){
+        return funcionarioRepository.findAll();
     }
 
 }
