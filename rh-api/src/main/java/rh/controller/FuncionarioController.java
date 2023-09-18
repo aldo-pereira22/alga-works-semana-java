3da92e7ec237c0ae58795330e0ba70e5a54743aa
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-09-18 12:41:54 -0300

    Atualizar FUNCIONARIO

--- rh-api/src/main/java/rh/controller/FuncionarioController.java
+++ rh-api/src/main/java/rh/controller/FuncionarioController.java
@@ -3,6 +3,7 @@ package rh.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rh.model.Funcionario;
 import rh.repository.FuncionarioRepository;
@@ -15,7 +16,7 @@ public class FuncionarioController {
 
     @Autowired
     private FuncionarioRepository funcionarioRepository;
-    
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -28,4 +29,22 @@ public class FuncionarioController {
         return funcionarioRepository.findAll();
     }
 
+    @GetMapping("/{funcionarioId}")
+    public ResponseEntity<Funcionario> buscar(@PathVariable Long funcionarioId){
+        return funcionarioRepository.findById(funcionarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{funcionarioId}")
+    public ResponseEntity<Funcionario> atualizar(@PathVariable Long funcionarioId,
+                                                 @RequestBody Funcionario funcionario){
+
+        if(!funcionarioRepository.existsById(funcionarioId)){
+            return ResponseEntity.notFound().build();
+        }
+        funcionario.setId(funcionarioId);
+        Funcionario funcionarioAtualizado = funcionarioRepository.save(funcionario);
+        return ResponseEntity.ok(funcionarioAtualizado);
+    }
 }
